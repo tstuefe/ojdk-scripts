9796e58520d1614d419606f7ec9264a9fe324de8
tstuefe <thomas.stuefe@gmail.com>
2018-10-27 18:26:46 +0200

    Change default to overwrite the highest previously exported webrev, not to create a new one. New option -i to increase webrev number.

--- upload-patch.py
+++ upload-patch.py
@@ -37,11 +37,14 @@ def run_command_and_return_stdout (command):
     trc('----')
     return stdout
 
+def build_patch_directory_path(patch_name):
+    return export_root_dir + '/' + patch_name
+
 def build_webrev_path(patch_name, webrev_number):
-    return export_root_dir + '/' + patch_name + '/webrev_' + str(webrev_number)
+    return build_patch_directory_path(patch_name) + '/webrev_' + str(webrev_number)
 
 def build_patch_path(patch_name):
-    return export_root_dir + '/' + patch_name + '/' + patch_name + '.patch'
+    return build_patch_directory_path(patch_name) + '/' + patch_name + '.patch'
 
 def user_confirm(question):
     print(question + " [y/n]: ")
@@ -65,18 +68,31 @@ parser = argparse.ArgumentParser(description='Upload a patch to cr.ojdk.j.n')
 # (Note: variable name seems to be the first encountered long (--) option name)
 parser.add_argument("-v", "--verbose", dest="is_verbose",
                     help="Debug output", action="store_true")
-parser.add_argument("-p", "--patch", dest="patch_mode", help="Make a patch (default: make a webrev)", action="store_true")
-parser.add_argument("-n", "--webrev-number", dest="webrevno", type=int, help="Webrev number. By default, numbers are incremented automatically in the export dir.")
-parser.add_argument("-o", "--omit-upload", dest="omit_upload", help="Omit upload - just export the patch or webrev to the export directory")
+parser.add_argument("-p", "--patch-mode", dest="patch_mode", help="Patch mode (creates a patch file using hg export)", action="store_true")
+parser.add_argument("-w", "--webrev-mode", dest="webrev_mode", help="Webrev mode (creates a webrev using webrev.ksh. This is the default.", action="store_true")
+parser.add_argument("-i", "--increase-webrev-number", dest="inc_webrev_number",
+                    help="Webrev mode only: When writing the webrev, increase the webrev number. By default, we overwrite the highest webrev.", action="store_true")
 parser.add_argument("-y", dest="yesyes", help="Anser YES automatically to all questions")
+parser.add_argument("-n", "--no-upload", dest="no_upload", help="Omit upload - just export the patch or webrev to the export directory", action="store_true")
 
 args = parser.parse_args()
 
 #---
 
+# sanity: -p and -w are mutually exclusive
+if args.patch_mode and args.webrev_mode:
+    sys.exit("Specify either -p or -w, but not both.")
+elif args.patch_mode:
+    is_patch_mode = True
+    is_webrev_mode = False
+else:
+    is_webrev_mode = True
+    is_patch_mode = False
+
+
 # sanity: -n makes only sense in webrev mode
-if args.webrevno is not None and args.patch_mode:
-    sys.exit("Option -n|--webrev-number only supported in webrev mode.")
+if args.inc_webrev_number == True and is_patch_mode:
+    sys.exit("Option -i only supported in webrev mode.")
 
 # run hg qapplied. That should give us exaclty one line, since we expect only one patch applied.
 qapplied_result = run_command_and_return_stdout(['hg', 'qapplied']).splitlines()
@@ -100,42 +116,32 @@ if not pathlib.Path('../../export').exists():
     sys.exit('export directory not found (../../export)')
 
 # create export directory
-pathlib.Path('../../export/' + patch_name).mkdir(parents=True, exist_ok=True) 
+patch_directory = build_patch_directory_path(patch_name)
+pathlib.Path(patch_directory).mkdir(parents=True, exist_ok=True) 
 
 # Calculate the full export path. For a patch, this is the patch to the (unnumbered) patch file. For webrevs,
 # the patch to the numbered webrev directory.
-full_export_path = ""
-    
-if args.patch_mode:
+
+if is_patch_mode:
     full_export_path = build_patch_path(patch_name)
 else:
     # webrev mode:
-    # by default, we search in the export directory for the highest numbered webrev
-    # and add one. If -n is given, we use that number.
+    # First, find existing highest webrev in export dir
     webrev_number = 0
-    if args.webrevno is None:
-        while pathlib.Path(build_webrev_path(patch_name, webrev_number)).exists():
-            webrev_number += 1
-    else:
-        # override number
-        webrev_number = args.webrevno
+    while pathlib.Path(build_webrev_path(patch_name, webrev_number)).exists():
+        trc("Found pre-existing: " + build_webrev_path(patch_name, webrev_number))
+        webrev_number += 1
+    # by default, we overwrite the highest one. Unless -i is specified, then we increase.
+    if not args.inc_webrev_number == True:
+        if webrev_number > 0:
+            webrev_number -= 1
     full_export_path = build_webrev_path(patch_name, webrev_number)
 
 trc("Export path is: " + full_export_path)
 
-if not args.patch_mode:
-    # Sanity checks for webrev mode:
-    if args.webrevno is not None:
-        # if number is forced: there must not exist a webrev with a higher number in the export dir
-        # (e.g. -n 10 should not create webrev_10 if there already exist webrev_11++) since this
-        # most probably is a user error
-        next_theoretical_webrev = build_webrev_path(patch_name, webrev_number + 1)
-        if pathlib.Path(next_theoretical_webrev).exists():
-            sys.exit('webrev number forced to ' + str(args.webrevno) + " but higher numbered webrevs found - aborting.")
-
-# if there is a preexisting export: ask before deleting
+# Delete pre-existing. But ask beforehand.
 if pathlib.Path(full_export_path).exists():
-    if args.patch_mode:
+    if is_patch_mode:
         user_confirm('Remove pre-existing patch: ' + full_export_path)
         trc("Removing pre-existing patch at " + full_export_path + "...")
         pathlib.Path(full_export_path).unlink()
@@ -150,7 +156,7 @@ else:
     trc("No preexisting patch/webrev found - OK.")
 
 # Produce the webrev, patch:
-if args.patch_mode:
+if is_patch_mode:
     trc("Exporting patch...")
     result = run_command_and_return_stdout(["hg", "export", "-o", full_export_path])
     print(result)
@@ -163,7 +169,7 @@ else:
 
 # upload to remote: For simplicity, I just transfer the whole patch dir, regardless if that transfers
 # older webrevs too. rsync will only transfer stuff not remote already.
-if args.omit_upload:
+if args.no_upload:
     trc("--export-only specified: omitting upload.")
 else:
     trc("Uploading patch...")
